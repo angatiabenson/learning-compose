5004e2431096f6aef32b525ed820a69bae19ae44
Angatia Benson <bensonetia@gmail.com>
2024-05-17 06:19:36 -0500

    Text styling

--- app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
+++ app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
@@ -29,7 +29,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -183,9 +191,53 @@ fun ImageCardLayout() {
     }
 }
 
+@Composable
+fun TextStylingLayout() {
+    val fontFamily = FontFamily(
+        Font(R.font.light_more, FontWeight.Thin),
+        Font(R.font.light, FontWeight.Light),
+        Font(R.font.normal, FontWeight.Normal),
+        Font(R.font.bold, FontWeight.Bold),
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF101010))
+            .padding(12.dp)
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ){
+                    append("J")
+                }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ){
+                    append("C")
+                }
+                append("ompose")
+            },
+            color = Color.White,
+            fontSize = 20.sp,
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ImageCardLayout()
+    TextStylingLayout()
 }
