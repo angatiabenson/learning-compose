a09b914a122ee04335bbc975becfcb075cc875c2
Angatia Benson <bensonetia@gmail.com>
2024-05-17 04:42:45 -0500

    Rows, Columns and Basic sizing.

--- app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
+++ app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ke.co.banit.leaningcompose.ui.theme.LeaningComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +45,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LeaningComposeTheme {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
         Greeting("Android")
+        Greeting("Bantos Ben")
     }
 }
