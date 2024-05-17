3da9eaa5ff00834a6e946f095ec70805d2c828f6
Angatia Benson <bensonetia@gmail.com>
2024-05-17 05:01:55 -0500

    Modifiers

--- app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
+++ app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ke.co.banit.leaningcompose.ui.theme.LeaningComposeTheme
@@ -42,17 +48,59 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainLayout() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
+            .padding(12.dp)
     ) {
-        Greeting("Android")
-        Greeting("Bantos Ben")
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(3.dp, color = Color.Black)
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting("Android")
+            Spacer(modifier = Modifier.height(10.dp))
+            Greeting("Angatia Benson")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(3.dp, color = Color.Black)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column(
+                modifier = Modifier
+                    .border(3.dp, color = Color.Black)
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Greeting("Android")
+                Spacer(modifier = Modifier.height(10.dp))
+                Greeting("Angatia Benson")
+            }
+            Column(
+                modifier = Modifier
+                    .border(3.dp, color = Color.Black)
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Greeting("Android")
+                Spacer(modifier = Modifier.height(10.dp))
+                Greeting("Angatia Benson")
+            }
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainLayout()
 }
