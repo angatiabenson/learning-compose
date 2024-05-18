16ab22b2445b72543230b819154090160a4f2aaf
Angatia Benson <bensonetia@gmail.com>
2024-05-18 04:53:01 -0500

    intro into simple animantions

--- app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
+++ app/src/main/java/ke/co/banit/leaningcompose/MainActivity.kt
@@ -1,94 +1,65 @@
 package ke.co.banit.leaningcompose
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val constraints = ConstraintSet {
-                val greenBox = createRefFor("greenbox")
-                val redBox = createRefFor("redbox")
-                val blueBox = createRefFor("bluebox")
-                val title = createRefFor("title")
-
-                constrain(title) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    width = Dimension.fillToConstraints
-                    height = Dimension.wrapContent
-                }
-                constrain(greenBox) {
-                    top.linkTo(title.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(redBox.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                constrain(redBox) {
-                    top.linkTo(title.bottom)
-                    start.linkTo(greenBox.end)
-                    end.linkTo(blueBox.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                constrain(blueBox) {
-                    top.linkTo(title.bottom)
-                    start.linkTo(redBox.end)
-                    end.linkTo(parent.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
+            var sizeState by remember {
+                mutableStateOf(200.dp)
             }
-
-            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-                Text(
-                    text = "App Title",
-                    style = MaterialTheme.typography.headlineLarge,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                        .layoutId("title"),
-                    textAlign = TextAlign.Center
+            val size by animateDpAsState(
+                targetValue = sizeState, label = "", animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioHighBouncy, stiffness = Spring.StiffnessLow
                 )
-                Box(
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .background(Color.Green)
-                        .layoutId("greenbox"))
-                Box(
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .background(Color.Red)
-                        .layoutId("redbox"))
-                Box(
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .background(Color.Blue)
-                        .layoutId("bluebox"))
+            )
+            val infiniteTransition = rememberInfiniteTransition(label = "")
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1000), repeatMode = RepeatMode.Reverse
+                ), label = ""
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(color),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increase size")
+                }
             }
         }
     }
